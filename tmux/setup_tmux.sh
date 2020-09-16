6651b4ead25d1e17994ca194a2c82ee2a07044ad
Basti-Fantasti <bastian.teufel@gmail.com>
2020-09-16 11:43:06 +0200

    added apt packages

--- tmux/setup_tmux.sh
+++ tmux/setup_tmux.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# 
 sudo apt install -y libevent-dev libutempter0 libutempter-dev ncurses-\* urlview xsel fonts-powerline
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
