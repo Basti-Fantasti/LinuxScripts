56c55bbe44599bb29e69837ab596fad492982c65
Basti-Fantasti <bastian.teufel@gmail.com>
2020-09-16 14:34:39 +0200

    adjusted tmux conf and apt packages

--- tmux/setup_tmux.sh
+++ tmux/setup_tmux.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # 
-sudo apt install -y tmux libevent-dev libutempter0 libutempter-dev ncurses-\* urlview xsel fonts-powerline
+sudo apt install -y tmux libevent-dev libutempter0 libutempter-dev urlview xsel fonts-powerline
+sudo apt install -y ncurses-\*
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
